7bd08ebe62e852b86f3797b5d1a297123ac812aa
Matthew Pham <matto475@gmail.com>
2018-08-27 09:17:41 +0000

    WIP CODE combining address with fetching

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /* eslint-disable  func-names */
 /* eslint-disable  no-console */
 
-const Alexa = require('ask-sdk-core');
+const Alexa = require('ask-sdk');
+const PERMISSIONS = ['read::alexa:device:all:address'];
+
 
 const GetRemoteDataHandler = {
   canHandle(handlerInput) {
@@ -10,19 +12,56 @@ const GetRemoteDataHandler = {
       && handlerInput.requestEnvelope.request.intent.name === 'GetRemoteDataIntent');
   },
   async handle(handlerInput) {
+    const { requestEnvelope, serviceClientFactory, responseBuilder } = handlerInput;
+
     let outputSpeech = 'This is the default message.';
     var longitude ='';
     var latitude='';
     var listOfFuel= '';
 
-    var nominalString = "https://nominatim.openstreetmap.org/search?q="
-    + "2902+West+Diana+avenue,+phoenix"  //this is the address
-    + "&format=jsonv2&polygon=1&addressdetails=1";
+    var nominalString = "https://nominatim.openstreetmap.org/search?q=";
+   // + "2902+West+Diana+avenue,+phoenix" + //this is the address
+    var formatTypee = "&format=jsonv2&polygon=1&addressdetails=1";
 
     var overpassString = "http://overpass-api.de/api/interpreter?data=[out:json];(node[%22amenity%22=%22fuel%22]"
     + "(around:8046.72,"    //this is roughly 5 miles
-    + "33.562883566207,-112.121428707594" //this is the actual lat, lon
-    + "););out;%3E;";
+    //+ "33.562883566207,-112.121428707594" + //this is the actual lat, lon
+    var overpassOut = "););out;%3E;";
+
+    //this fetches the user address
+    const consentToken = requestEnvelope.context.System.user.permissions
+    && requestEnvelope.context.System.user.permissions.consentToken;
+
+  if (!consentToken) {
+    return responseBuilder
+      .speak('Please enable Location permissions in the Amazon Alexa app.')
+      .withAskForPermissionsConsentCard(PERMISSIONS)
+      .getResponse();
+  }
+  try {
+    const { deviceId } = requestEnvelope.context.System.device;
+    const deviceAddressServiceClient = serviceClientFactory.getDeviceAddressServiceClient();
+    const address = await deviceAddressServiceClient.getFullAddress(deviceId);
+
+    console.log('Address successfully retrieved, now responding to user.');
+
+    let response;
+    if (address.addressLine1 === null && address.stateOrRegion === null) {
+      response = responseBuilder.speak('It looks like you don\'t have an address set. You can set your address from the companion app.').getResponse();
+    } else {
+      const ADDRESS_MESSAGE = `${'Here is your full address:' + address.addressLine1}, ${address.stateOrRegion}, ${address.postalCode}`;
+      address.addressLine1 = address.addressLine1.replace(/\s/g, "+"); //will replace all white spaces with +
+
+      response = responseBuilder.speak(ADDRESS_MESSAGE).getResponse();
+    }
+    return response;
+  } catch (error) {
+    if (error.name !== 'ServiceError') {
+      const response = responseBuilder.speak('Uh Oh. Looks like something went wrong.').getResponse();
+      return response;
+    }
+    throw error;
+  }
 
     //this geocodes the user address
     await getRemoteData('https://nominatim.openstreetmap.org/search?q=2902+West+Diana+avenue,+phoenix&format=jsonv2&polygon=1&addressdetails=1')
@@ -42,13 +81,11 @@ const GetRemoteDataHandler = {
       .then((response) => {
         const data = JSON.parse(response);
 
-
         for (let i = 0; i < 6; i++) {  //gets 5 nodes
           if(data.elements[i].tags.name != undefined ){
             listOfFuel = listOfFuel + ' , ' + data.elements[i].tags.name;
           }
         }
-       //   listOfFuel = data.elements[0].tags.name;
 
         outputSpeech = "The latitude is " + latitude + " The longitude is " + longitude + '. The list is '
         + listOfFuel; 
@@ -108,6 +145,24 @@ const SessionEndedRequestHandler = {
   },
 };
 
+const GetAddressError = {
+  canHandle(handlerInput, error) {
+    return error.name === 'ServiceError';
+  },
+  handle(handlerInput, error) {
+    if (error.statusCode === 403) {
+      return handlerInput.responseBuilder
+        .speak(messages.NOTIFY_MISSING_PERMISSIONS)
+        .withAskForPermissionsConsentCard(PERMISSIONS)
+        .getResponse();
+    }
+    return handlerInput.responseBuilder
+      .speak(messages.LOCATION_FAILURE)
+      .reprompt(messages.LOCATION_FAILURE)
+      .getResponse();
+  },
+};
+
 const ErrorHandler = {
   canHandle() {
     return true;
@@ -147,6 +202,6 @@ exports.handler = skillBuilder
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler
   )
-  .addErrorHandlers(ErrorHandler)
+  .addErrorHandlers(ErrorHandler,GetAddressError)
   .lambda();
 
