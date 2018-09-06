fbb76e0c58530b1a40d8d8d721ff2c8e1b5b1219
Matthew Pham <matto475@gmail.com>
2018-09-06 06:48:08 +0000

    +Changed the geocoder to mapquest, nomitim request not functioning when the script is run.

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 /* eslint-disable  no-console */
 
 const Alexa = require('ask-sdk');
-const PERMISSIONS = ['read::alexa:device:all:address'];
-
 
 const GetRemoteDataHandler = {
   canHandle(handlerInput) {
@@ -12,90 +10,59 @@ const GetRemoteDataHandler = {
       && handlerInput.requestEnvelope.request.intent.name === 'GetRemoteDataIntent');
   },
   async handle(handlerInput) {
-    const { requestEnvelope, serviceClientFactory, responseBuilder } = handlerInput;
-
     let outputSpeech = 'This is the default message.';
     var longitude ='';
     var latitude='';
     var listOfFuel= '';
 
-    var nominalString = "https://nominatim.openstreetmap.org/search?q=";
-   // + "2902+West+Diana+avenue,+phoenix" + //this is the address
-    var formatTypee = "&format=jsonv2&polygon=1&addressdetails=1";
+    //mapquest free tier allow for 15,000 requests a month.
+    var mapQuestGeocode = "http://www.smapquestapi.com/geocoding/v1/address?key=XSrWCuhRGcPPEYkYWIfwjIisN2vMyGct&location="
+    + "2902+West+Diana+avenue,phoenix"
 
+    //Overpass/openstreetmap is open source data so no limit in request.
     var overpassString = "http://overpass-api.de/api/interpreter?data=[out:json];(node[%22amenity%22=%22fuel%22]"
     + "(around:8046.72,"    //this is roughly 5 miles
-    //+ "33.562883566207,-112.121428707594" + //this is the actual lat, lon
-    var overpassOut = "););out;%3E;";
-
-    //this fetches the user address
-    const consentToken = requestEnvelope.context.System.user.permissions
-    && requestEnvelope.context.System.user.permissions.consentToken;
-
-  if (!consentToken) {
-    return responseBuilder
-      .speak('Please enable Location permissions in the Amazon Alexa app.')
-      .withAskForPermissionsConsentCard(PERMISSIONS)
-      .getResponse();
-  }
-  try {
-    const { deviceId } = requestEnvelope.context.System.device;
-    const deviceAddressServiceClient = serviceClientFactory.getDeviceAddressServiceClient();
-    const address = await deviceAddressServiceClient.getFullAddress(deviceId);
-
-    console.log('Address successfully retrieved, now responding to user.');
-
-    let response;
-    if (address.addressLine1 === null && address.stateOrRegion === null) {
-      response = responseBuilder.speak('It looks like you don\'t have an address set. You can set your address from the companion app.').getResponse();
-    } else {
-      const ADDRESS_MESSAGE = `${'Here is your full address:' + address.addressLine1}, ${address.stateOrRegion}, ${address.postalCode}`;
-      address.addressLine1 = address.addressLine1.replace(/\s/g, "+"); //will replace all white spaces with +
-
-      response = responseBuilder.speak(ADDRESS_MESSAGE).getResponse();
-    }
-    return response;
-  } catch (error) {
-    if (error.name !== 'ServiceError') {
-      const response = responseBuilder.speak('Uh Oh. Looks like something went wrong.').getResponse();
-      return response;
-    }
-    throw error;
-  }
+    + "33.562883566207,-112.121428707594" //this is the actual lat, lon
+    + "););out;%3E;";
 
     //this geocodes the user address
-    await getRemoteData('https://nominatim.openstreetmap.org/search?q=2902+West+Diana+avenue,+phoenix&format=jsonv2&polygon=1&addressdetails=1')
+    await getRemoteData('http://www.mapquestapi.com/geocoding/v1/address?key=XSrWCuhRGcPPEYkYWIfwjIisN2vMyGct&location=2902+West+Diana+avenue,phoenix')
       .then((response) => {
-        const data = JSON.parse(response);
-        latitude = data[0].lat;
-        longitude = data[0].lon;
-        //outputSpeech = "The latitude is " + data[0].lat + " The longitude is " + data[0].lon;        
+        var data = JSON.parse(response);
+
+        latitude = data.results[0].locations[0].displayLatLng.lat;
+        longitude = data.results[0].locations[0].displayLatLng.lng;
+        
       })
       .catch((err) => {
         //set an optional error message here
         //outputSpeech = err.message;
-      });
+      })
+    
 
       //this is a query to overpass api to get locations
       await getRemoteData('http://overpass-api.de/api/interpreter?data=[out:json];(node[%22amenity%22=%22fuel%22](around:8046.72,33.562883566207,-112.121428707594););out;%3E;')
       .then((response) => {
-        const data = JSON.parse(response);
+        var data = JSON.parse(response);
+
 
         for (let i = 0; i < 6; i++) {  //gets 5 nodes
           if(data.elements[i].tags.name != undefined ){
             listOfFuel = listOfFuel + ' , ' + data.elements[i].tags.name;
           }
         }
-
-        outputSpeech = "The latitude is " + latitude + " The longitude is " + longitude + '. The list is '
-        + listOfFuel; 
+       //   listOfFuel = data.elements[0].tags.name;
 
       })
       .catch((err) => {
         //set an optional error message here
         //outputSpeech = err.message;
-      });
+      })
+      
 
+
+    outputSpeech = "The latitude is " + latitude + " The longitude is " + longitude + '. The list is '
+    + listOfFuel; 
 
     return handlerInput.responseBuilder
       .speak(outputSpeech)
@@ -145,24 +112,6 @@ const SessionEndedRequestHandler = {
   },
 };
 
-const GetAddressError = {
-  canHandle(handlerInput, error) {
-    return error.name === 'ServiceError';
-  },
-  handle(handlerInput, error) {
-    if (error.statusCode === 403) {
-      return handlerInput.responseBuilder
-        .speak(messages.NOTIFY_MISSING_PERMISSIONS)
-        .withAskForPermissionsConsentCard(PERMISSIONS)
-        .getResponse();
-    }
-    return handlerInput.responseBuilder
-      .speak(messages.LOCATION_FAILURE)
-      .reprompt(messages.LOCATION_FAILURE)
-      .getResponse();
-  },
-};
-
 const ErrorHandler = {
   canHandle() {
     return true;
@@ -178,7 +127,7 @@ const ErrorHandler = {
 };
 
 //MATTHEW COMMENT refer to https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html for error codess
-const getRemoteData = function (url) {
+async function getRemoteData (url) {
   return new Promise((resolve, reject) => {
     const client = url.startsWith('https') ? require('https') : require('http');
     const request = client.get(url, (response) => {
@@ -202,6 +151,6 @@ exports.handler = skillBuilder
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler
   )
-  .addErrorHandlers(ErrorHandler,GetAddressError)
+  .addErrorHandlers(ErrorHandler)
   .lambda();
 
